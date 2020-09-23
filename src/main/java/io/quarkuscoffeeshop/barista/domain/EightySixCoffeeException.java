6dbe14b8b6e3c32c067bfc765e81d7ef9bb21355
Jeremy Davis <jeremyrdavis@mac.com>
2020-09-23 13:50:14 -0400

    added SNAPSHOT

--- src/main/java/io/quarkuscoffeeshop/barista/domain/EightySixCoffeeException.java
+++ src/main/java/io/quarkuscoffeeshop/barista/domain/EightySixCoffeeException.java
@@ -8,7 +8,7 @@ import java.util.Collection;
 
 public class EightySixCoffeeException extends Exception {
 
-    public Collection<EightySixEvent> getEvensa() {
+    public Collection<EightySixEvent> getEvents() {
         return Arrays.asList(new EightySixEvent(Item.COFFEE_BLACK), new EightySixEvent(Item.COFFEE_WITH_ROOM));
     }
 
